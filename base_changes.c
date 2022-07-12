523120b55079a48f6fbaeb5f509aaa5658b6be7c
Vincent Marsh <91901144+marshvin@users.noreply.github.com>
2022-07-12 15:51:07 +0300

    Update base_changes.c

--- base_changes.c
+++ base_changes.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 #include <stdio.h>
 
@@ -723,47 +723,23 @@ int print_heX(va_list list)
   write_base(rev_hex);
   
   free(hex_rep);
-  
-  free(rev_hex);
-  
-  return (len);
-  
+free(rev_hex); 
+return (len); 
 }
-
-
-
 /**
-
  * hex_check - Checks which hex function is calling it
-
  * @num: Number to convert into letter
-
  * @x: Tells which hex function is calling it
-
  * Return: Ascii value for a letter
-
- */
-
-int hex_check(int num, char x)
-  
+*/
+int hex_check(int num, char x)  
 {
-  
-  char *hex = "abcdef";
-  
-  char *Hex = "ABCDEF";
-  
-
-  
-  num = num - 10;
-  
-  if (x == 'x')
-    
-    return (hex[num]);
-  
-  else
-    
-    return (Hex[num]);
-  
-  return (0);
-  
+char *hex = "abcdef";
+char *Hex = "ABCDEF";
+num = num - 10;
+if (x == 'x')
+return (hex[num]);
+else
+return (Hex[num]);
+return (0);
 }g
